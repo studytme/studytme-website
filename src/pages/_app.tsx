b50e085778f2b3256c627152cf88b7b45aa62de0
Alexandr Hanák <ahs@seznam.cz>
2022-06-07 19:17:29 +0200

    added schedule page, seo, etc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,37 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        {/* General */}
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Home of the StudyTme community. Follow Giulia on Twitch, Youtube and TikTok. Also join us on Discord."
-        />
-        <link rel="canonical" href="https://studytme.com/" />
-        <meta name="robots" content="index, follow" />
-
-        {/* OpenGraph */}
-        <meta property="og:title" content="StudyTme community website" />
-        <meta
-          property="og:description"
-          content="Home of the StudyTme community. Follow Giulia on Twitch, Youtube and TikTok. Also join us on Discord."
-        />
-        <meta property="og:image" content="https://studytme.com/images/transparentG.png" />
-        <meta property="og:url" content="https://studytme.com" />
-        <meta property="og:site_name" content="StudyTme" />
-
-        {/* Twitter */}
-        <meta name="twitter:title" content="StudyTme community website" />
-        <meta
-          name="twitter:description"
-          content="Home of the StudyTme community. Follow Giulia on Twitch, Youtube and TikTok. Also join us on Discord."
-        />
-        <meta name="twitter:url" content="https://studytme.com/images/transparentG.png" />
-        <meta name="twitter:site" content="@StudyTme" />
-        <meta name="twitter:creator" content="@StudyTme" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:image" content="https://studytme.com/images/transparentG.png" />
-        <meta name="twitter:image:alt" content="Photo of StudyTme G" />
+        <title>StudyTme</title>
       </Head>
       <Component {...pageProps} />
     </Layout>
