05644381c390e3c9536e66a2a142e648adae5687
Mikiyas Tibebu <mikitebe123@gmail.com>
2022-06-10 17:06:18 +0300

    removed Next Image tag

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import streamSchedulePhoto from "../../public/images/schedule.png";
-
 const Schedule = () => {
   return (
     <div className="flex flex-col items-center">
-      <Image src={streamSchedulePhoto} className="max-w-xl" alt="stream schedule" />
+      <img src="/images/schedule.png" className="max-w-xl" alt="stream schedule" />
     </div>
   );
 };
