3cd0eb9abd002b9078b4a8d2bf07e5cfe5a4838f
Giulia Mazza <mazzagiulia0@gmail.com>
2022-07-28 15:56:39 +0200

    no 8080 in redirect checkin

--- src/pages/checkin.tsx
+++ src/pages/checkin.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const Checkin = () => {
   useEffect(() => {
-    window.location.replace("http://49.12.100.102:8080/checkin");
+    window.location.replace("http://49.12.100.102/checkin");
   }, []);
   return <div className="flex flex-col items-center justify-center h-full">Redirecting...</div>;
 };
